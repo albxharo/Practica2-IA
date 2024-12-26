4a986e79fed75e1c9b7627bc7d389bfa5be1dc26
albxharo <131656289+albxharo@users.noreply.github.com>
2024-12-26 11:42:51 +0100

    imports

---
+++ PRACTICA2-IA/Assets/Scripts/GrupoH/Estado.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GrupoH { 
+    public class Estado : MonoBehaviour
+    {
+        private
+            bool enemigoHabitacion;
+            bool visible;
+            Type salud;
+            bool medPackCerca;
+
+        public 
+
+            // Start is called before the first frame update
+            void Start()
+            {
+        
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+        
+            }
+}   }
+
